dc9998abf5186dcb31c5cc26d98ebe53ad8d8af2
Sri Velicheti <velicheti.harsha@gmail.com>
2016-12-28 14:01:45 -0500

    Arrays

--- src/Arrays/ArraysCPP/ArraysCPP.cpp
+++ src/Arrays/ArraysCPP/ArraysCPP.cpp
@@ -32,18 +32,63 @@ int max_sum(int A[], int N)
 	//Your code here
 }
 
+
+void Move(int pos, int* arr , int placeHolder)
+{
+	
+	if (arr[pos] == pos)
+		return;
+
+	if (arr[arr[pos]] == placeHolder)
+	{
+		//The value in the position to be moved to is already moved away to a new position.
+		arr[arr[pos]] = -1 * pos; //use negative values to denote an already moved element so that this will not get moved again.
+		arr[pos] = placeHolder;
+	}
+	else
+	{
+		auto tempVal = arr[pos];
+		arr[pos] = placeHolder;
+		Move(tempVal, arr, placeHolder);
+		arr[tempVal] = -1 * pos;
+	}
+
+	
+}
+
+void ReArrangeArray(int* a, int n)
+{
+	//Placeholder to denote a moved and empty position in an array. We are taking the size of array  as there can be no value
+	//equal to the length of array in the array it self.
+	int placeHolder = -1 * n;
+	for (int i = 0; i < n; ++i)
+	{
+		if(a[i] != i && a[i] >0)
+		{
+			Move(i, a, placeHolder);
+		}
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		if(a[i] < 0)
+			a[i] = -1 * a[i];
+	}
+}
+
+
+
 int main()
 {
 	
-	int* harray = new int[2]{100,200};
-	int sarray[2]{ 400,500 };
+	//int* harray = new int[5]{ 2,0,1,4,3 };
 
-	int* psarray = sarray;
-	++harray;
-	std::cout << *harray << endl;
-	(psarray)++;
-	std::cout << *psarray << endl;
- 	int temp;
+	int* harray = new int[7]{ 0,2,6,3,1,5,4 };
+	ReArrangeArray(harray, 7);
+	for (int i = 0; i < 7; ++i)
+	{
+		cout << harray[i] << " ";
+	}
+	int temp;
 	std::cin >> temp;
     return 0;
 }
